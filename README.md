a8da4e375d022e0181cea2e64eed0b0cb9e98258
Anoop Pant <apant@deloitte.com>
2026-03-24 16:17:10 -0400

    environment setup

---
+++ README.md
@@ -0,0 +1,1 @@
+https://deloittedevelopment.udemy.com/course/langchain/learn/lecture/52009171#overview
---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+# load environment variables from .env file
+load_dotenv()
+
+
+def main():
+    print("Hello from langchain-course!")
+
+
+if __name__ == "__main__":
+    main()
